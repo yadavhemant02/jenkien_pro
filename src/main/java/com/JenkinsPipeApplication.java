a466d425722255c970c326de15bffe9ef687375f
hemantyadav02 <hemant.yadav02@nagarro.com>
2023-05-01 18:15:53 +0530

    first commit

---
+++ src/main/java/com/JenkinsPipeApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipeApplication.class, args);
+	}
+
+}
